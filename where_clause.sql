41f8c3914a53030daeb49b4b2182c2214fadd030
ganendra8 <168290956+ganendra8@users.noreply.github.com>
2024-07-09 20:07:43 -0700

    Create where_clause.sql

---
+++ where_clause.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE student;
+use student;
+
+CREATE TABLE students(
+std_id INT PRIMARY KEY,
+std_name VARCHAR(20),
+std_address VARCHAR(20),
+dept_id INT,
+age INT
+);
+
+INSERT INTO students VALUES
+(10, 'Maya', 'Palpa', 1, 21),
+(11, 'Hari', 'ktm', 2, 22),
+(12, 'Ram', 'ktm', 1, 19),
+(13, 'Sira', 'ktm', 3, 33),
+(14, 'Pinky', 'Palpa', 4, 23),
+(15, 'Gita', 'Pokhara', 2, 22),
+(16, 'Rita', 'ktm', 1, 49);
+
+SELECT std_name, std_address FROM students;
+
+SELECT std_name, std_address FROM students WHERE dept_id = 1;
+
+SELECT std_name, std_address FROM students WHERE std_address = 'ktm';
